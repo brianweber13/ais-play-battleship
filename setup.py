0c12d4aa772f6feb4d7757972cdd2cb9a8c96ed4
Brian Weber <brian.weber1337@gmail.com>
2018-08-10 15:10:07 -0400

    added to make this a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ais_play_battleship',
+      version='0.1',
+      description="""a program for pitting two battleship-playing ais against
+      each other""",
+      url='https://github.com/brianweber13/ais-play-battleship',
+      author='Brian Weber',
+      author_email='brian.weber1337@gmail.com',
+      license='GPLv3')
